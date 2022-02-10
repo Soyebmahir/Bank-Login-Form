6bcf3068b640b5d8059e9969b651a35825111900
Soyeb <tssoyeb@gmail.com>
2022-02-11 01:09:43 +0600

    add in deposit

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,20 @@
+
+//deposit and add in the deposit section
 document.getElementById('deposit-button').addEventListener('click',()=>{
 
-    console.log('clicked')
+const depositInput =document.getElementById('deposit-input');
+const depositAmount = depositInput.value ;
+const amountTotal = document.getElementById('amount-total')
+amountTotal.innerText=depositAmount;
+depositInput.value='';
+})
+
+//withdraw and add in the withdraw section
+document.getElementById('deposit-button').addEventListener('click',()=>{
+
+const depositInput =document.getElementById('deposit-input');
+const depositAmount = depositInput.value ;
+const amountTotal = document.getElementById('amount-total')
+amountTotal.innerText=depositAmount;
+depositInput.value='';
 })
