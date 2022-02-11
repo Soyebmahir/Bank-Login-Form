55130312dc632995e18f7d142856bfbf44221209
Soyeb <tssoyeb@gmail.com>
2022-02-11 06:21:42 +0600

    done

--- js/banking.js
+++ js/banking.js
@@ -1,20 +1,46 @@
 
 //deposit and add in the deposit section
-document.getElementById('deposit-button').addEventListener('click',()=>{
+document.getElementById('deposit-button').addEventListener('click',() =>{
 
 const depositInput =document.getElementById('deposit-input');
+
 const depositAmount = depositInput.value ;
-const amountTotal = document.getElementById('amount-total')
-amountTotal.innerText=depositAmount;
+
+const amountTotal = document.getElementById('amount-total') 
+
+
+
+const previousAmountTotal = amountTotal.innerText;
+const currentAmountTotal = parseFloat( previousAmountTotal) + parseFloat( depositAmount);
+amountTotal.innerText= currentAmountTotal;
+
+//from now its all about balance
+
+const balanceTotal =document.getElementById('balance-total');
+const previousBalanceTotal =balanceTotal.innerText;
+const currentBalance=parseFloat(previousBalanceTotal) +parseFloat(depositAmount);
+balanceTotal.innerText =currentBalance;
+
+
 depositInput.value='';
 })
 
 //withdraw and add in the withdraw section
-document.getElementById('deposit-button').addEventListener('click',()=>{
+document.getElementById('withdraw-button').addEventListener('click',()=>{
 
-const depositInput =document.getElementById('deposit-input');
-const depositAmount = depositInput.value ;
-const amountTotal = document.getElementById('amount-total')
-amountTotal.innerText=depositAmount;
-depositInput.value='';
+const withdrawInput =document.getElementById('withdraw-input');
+const withdrawAmount = withdrawInput.value ;
+const withdrawTotal = document.getElementById('withdraw-total')
+const previousWithdrawAmount = withdrawTotal.innerText;
+
+const currentWithdrawTotal =parseFloat(previousWithdrawAmount) +parseFloat( withdrawAmount);
+withdrawTotal.innerText= currentWithdrawTotal ;
+
+const balanceTotal =document.getElementById('balance-total');
+const previousBalanceTotal =balanceTotal.innerText;
+
+const currentBalance=parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
+balanceTotal.innerText =currentBalance;
+
+withdrawInput.value='';
 })
